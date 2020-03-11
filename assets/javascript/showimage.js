efc934cec0ec5796ad18ff413e34345cd911b2e0
hoge114514 <hogehoge@.localdomain>
2020-03-11 14:34:03 +0900

    Canvasに画像を描写する機能を追加

--- assets/javascript/showimage.js
+++ assets/javascript/showimage.js
@@ -1,39 +1,67 @@
 function OnFileSelect(inputElement) {
     // ファイルリストを取得
-    var fileList = inputElement.files;
+    let fileList = inputElement.files;
 
     // ファイルの数を取得
-    var fileCount = fileList.length;
+    let fileCount = fileList.length;
 
-    var loadCompleteCount = 0;
-    var imageList = "";
+    //正方形の大きさはウィンドウ依存
+    let canvasWidth = window.innerWidth*0.1;
+
+    //canvas初期化
+    $('.tiles').empty();
+
 
     // 選択されたファイルの数だけ処理する
-    for (var i = 0; i < fileCount; i++) {
-
-        // FileReaderを生成
-        var fileReader = new FileReader();
+    for (let i = 0; i < fileCount; i++) {
 
         // ファイルを取得
-        var file = fileList[i];
+        let file = fileList[i];
+
+        // 画像ファイル以外の場合は処理を終了する
+        if(!file.type.match(/^image\/(png|jpeg|gif)$/)) return ;
+        
+        // <canvas>タグの追加
+        $('.tiles').append("<canvas></canvas>\r\n");
 
         // 読み込み完了時の処理を追加
-        fileReader.onload = function () {
+        loadFile(file).then(function (response){ 
+            //canvas設定
+            let canvas = $('.tiles').children()[i];
+            console.log(toString.call(canvas));   
+            canvas.width = canvasWidth;
+            canvas.height = canvasWidth;
 
-            // <li>,<img>タグの生成
-            imageList += "<li><img src=\"" + this.result + "\" /></li>\r\n";
+            let ctx = canvas.getContext('2d');
 
-            // 選択されたファイルすべの処理が完了したら、<ul>タグに流し込む
-            if (++loadCompleteCount == fileCount) {
-
-                // <ul>タグに<li>,<img>を流し込む
-                document.getElementById("ID001").innerHTML = imageList;
-            }
-        };
-
-        // ファイルの読み込み(Data URI Schemeの取得)
-        fileReader.readAsDataURL(file);
+            loadImage(response).then(function(response){
+                ctx.clearRect(0,0,canvasWidth,canvasWidth);
+                ctx.drawImage(response,0,0,canvasWidth,canvasWidth);
+            }).catch(function(e){
+                console.log(e,"Failed to load image")
+            })
+        }).catch(function(e){
+            console.log(e,"Failed to load file");
+        })
     }
+}
+
+function loadFile(file) {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.onload = (event) => resolve(event.target.result);
+      fileReader.onerror = (e) => reject(e);
+      fileReader.readAsDataURL(file);
+    });
+}
+
+function loadImage(src) {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = (e) => reject(e);
+      img.src = src;
+    });
 }
 
 $(document).on('change', '.image', function () {
